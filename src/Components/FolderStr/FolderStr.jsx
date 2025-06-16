5f78d857a38e55300549bfbf189dea96aac2a2a9
Akash Kumar <47330573+Akash1258@users.noreply.github.com>
2025-06-16 18:58:55 +0000

    add file folder changes

--- src/Components/FolderStr/FolderStr.jsx
+++ src/Components/FolderStr/FolderStr.jsx
@@ -2,19 +2,30 @@ import { useState } from "react";
 import { Folder } from "./Folder";
 import { File } from "./File";
 
-export const FolderStr = ({ node, margin }) => {
+export const FolderStr = ({ node, margin, handleAddNew }) => {
   const [isExpanded, setIsExpanded] = useState(false);
   return (
     <div key={node?.id}>
       {node?.isFolder ? (
         <>
-          <div onClick={() => setIsExpanded((prev) => !prev)}>
-            <Folder name={node?.name} margin={margin} />
+          <div>
+            <Folder
+              name={node?.name}
+              margin={margin}
+              handleAddNew={handleAddNew}
+              id={node?.id}
+              setIsExpanded={setIsExpanded}
+            />
           </div>
           {isExpanded && (
             <div key={node?.id}>
               {node?.children?.map((item) => (
-                <FolderStr key={item?.id} node={item} margin={margin + 10} />
+                <FolderStr
+                  key={item?.id}
+                  node={item}
+                  margin={margin + 10}
+                  handleAddNew={handleAddNew}
+                />
               ))}
             </div>
           )}
--- src/Components/FolderStr/FolderStrContainer.jsx
+++ src/Components/FolderStr/FolderStrContainer.jsx
@@ -4,10 +4,34 @@ import { folderData } from "./constant";
 
 export const FolderStrContainer = () => {
   const [data, setData] = useState(folderData);
+
+  const handleAddNew = (parentId, itemToAdd) => {
+    let updated = false;
+    const findAndAddNode = (node) => {
+      return node?.map((item) => {
+        if (updated) return item;
+
+        if (item?.id === parentId) {
+          updated = true;
+          return { ...item, children: [...(item.children || []), itemToAdd] };
+        } else if (item?.children) {
+          return {
+            ...item,
+            children: findAndAddNode(item.children),
+          };
+        }
+        return item;
+      });
+    };
+
+    const updatedNode = findAndAddNode(data);
+    setData([...(updatedNode || [])]);
+  };
+
   return (
     <div>
       {data?.map((node) => (
-        <FolderStr node={node} margin={0} />
+        <FolderStr node={node} margin={0} handleAddNew={handleAddNew} />
       ))}
     </div>
   );
